8bae3c3fa6790d4c53a71b9e8fa03fd031be27b5
Felipe Wisniewski <felipe.gomes@deliverit.com.br>
2019-07-10 17:25:16 -0300

    social sharing mock

--- social-sharing/src/app/home/home.page.ts
+++ social-sharing/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
 
 @Component({
   selector: 'app-home',
@@ -9,9 +10,37 @@ export class HomePage {
 
   imgSrc = 'https://s3-sa-east-1.amazonaws.com/bancoimagens.com.br/posts/8246-355-11079-29452.jpg'
 
-  constructor() { }
+  constructor(private socialSharing: SocialSharing) { }
 
   share() {
-    console.log(this.imgSrc)
+    // this.socialSharing.shareViaFacebook('Sharing Facebook', null, this.imgSrc, () => { console.log('share ok') }, () => { console.log('error share') })
+    this.socialSharing.shareViaFacebook('Sharing Facebook', null, this.imgSrc)
   }
+
+
+
+  // Twitter
+  // <!-- unlike most apps Twitter doesn't like it when you use an array to pass multiple files as the second param -->
+  // shareViaTwitter('Message via Twitter')">message via Twitter</button>
+  // shareViaTwitter('Message and link via Twitter', null /* img */, 'http://www.x-services.nl')">msg and link via Twitter</button>
+
+  // Facebook
+  // shareViaFacebook('Message via Facebook', null /* img */, null /* url */, function() {console.log('share ok')}, function(errormsg){alert(errormsg)})">msg via Facebook (with errcallback)</button>
+  // shareViaFacebookWithPasteMessageHint('Message via Facebook', null /* img */, null /* url */, 'Paste it dude!', function() {console.log('share ok')}, function(errormsg){alert(errormsg)})">msg via Facebook (with errcallback)</button>
+  
+  // Whitelisting Facebook in your app's .plist:
+  // <key>LSApplicationQueriesSchemes</key>
+  // <array>
+  //   <string>fb</string>
+  // </array>
+
+  // Instagram
+  // shareViaInstagram('Message via Instagram', 'https://www.google.nl/images/srpr/logo4w.png', function() {console.log('share ok')}, function(errormsg){alert(errormsg)})">msg via Instagram</button>
+
+  // Whatsapp
+  // shareViaWhatsApp('Message via WhatsApp', null /* img */, null /* url */, function() {console.log('share ok')}, function(errormsg){alert(errormsg)})">msg via WhatsApp (with errcallback)</button>
 }
+
+
+
+
