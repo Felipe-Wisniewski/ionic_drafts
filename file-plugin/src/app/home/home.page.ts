421a7ef96c4d53d9fc6c2a6135fbca6cf6f4b5d1
Felipe Wisniewski <felipe.gomes@deliverit.com.br>
2019-08-20 18:35:27 -0300

    home

--- file-plugin/src/app/home/home.page.ts
+++ file-plugin/src/app/home/home.page.ts
@@ -13,30 +13,32 @@ export class HomePage {
 
   constructor(private platform: Platform, private file: File) { }
 
-  onClick() {
-    if (this.platform.is('ios')) {
-      console.log(this.platform.ready())
-      console.log(this.file.applicationDirectory)
+  readFile(file: any) {
+    const reader = new FileReader()
+    reader.onloadend = () => {
+      const formData = new FormData()
+      const imgBlob = new Blob([reader.result], {
+        type: file.type
+      })
+      formData.append('file', imgBlob, file.name)
+      // this.uploadImageData(formData);
+      console.log(formData)
     }
+    reader.readAsArrayBuffer(file)
+  }
 
+  /*
     if (this.platform.is('android')) {
-
+ 
       if (this.platform.ready()) {
         this.file.checkDir(this.file.applicationDirectory, 'android')
       }
       console.log()
     }
-
-    if (this.platform.is('desktop')) {
-      console.log(this.platform.ready())
-      console.log(this.file.applicationDirectory)
-    }
-
-
-    /*     this.file.checkDir(this.file.dataDirectory, 'Documents').then((it) => {
+            this.file.checkDir(this.file.dataDirectory, 'Documents').then((it) => {
           console.log('Directory exists', it)
         }).catch(err => {
           console.log('Directory doesnt exist', err)
-        }) */
-  }
+        }) 
+        */
 }
